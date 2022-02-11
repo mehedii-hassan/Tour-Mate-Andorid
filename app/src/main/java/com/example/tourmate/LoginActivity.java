76921d7a71161f2988c65a3280e0e5ba9b81e064
Mehedi-Hassan <mehedii.hassan01@gmail.com>
2022-02-11 23:06:35 +0600

    Upload project

---
+++ app/src/main/java/com/example/tourmate/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.example.tourmate;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    AppCompatButton button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        button = findViewById(R.id.btnLogin);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,SignUpActivity.class));
+            }
+        });
+
+
+
+    }
+}
